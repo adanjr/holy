f1bea0f14f82de3eddafc4e1ec05f90a132718c4
Adan Juarez <adanjr@gmail.com>
2026-07-02 20:17:51 -0600

    main Fix in paths

--- server/index.ts
+++ server/index.ts
@@ -90,7 +90,7 @@ async function main() {
   const remotionBundleUrl = REMOTION_SERVE_URL
     ? REMOTION_SERVE_URL
     : await bundle({
-        entryPoint: path.resolve("../remotion/index.ts"),
+        entryPoint: path.resolve("./remotion/index.ts"),
         onProgress(progress) {
           console.info(`Bundling Remotion project: ${progress}%`);
         },
